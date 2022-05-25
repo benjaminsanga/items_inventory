73fe1393eb98c0a9ed8b8f8714139cba5a222c90
Benjamin <benjotaiba@gmail.com>
2022-05-25 13:10:00 +0100

    comments and removed StrictMode for onetime render

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,22 @@ import ItemsList from './components/ItemsList';
 
 function App() {
 
-  const [item, setItem] = useState('')
-  const [quantity, setQuantity] = useState(0)
-  const dispatch = useAppDispatch()
+  const [item, setItem] = useState('') // form item hook 
+  const [quantity, setQuantity] = useState(0) // form item quantity hook
+  const dispatch = useAppDispatch() // hook for dispatching actions to store
 
+  // hook to sync locally store items with redux store
   useEffect(() => {
-    for (let storageItem in localStorage) { 
-      if (new Date(storageItem).toString().match(/[0-9]/g) === null) continue
+    for (let storageItemKey in localStorage) { // get each item key in localstorage
 
-      let item: { item: string, quantity: number, date: string } = JSON.parse(localStorage.getItem(storageItem) as string)
+      // use the item key to check for valid date
+      // using the regex for numbers technique
+      if (new Date(storageItemKey).toString().match(/[0-9]/g) === null) continue
+
+      // get item object from the stored value
+      let item: { item: string, quantity: number, date: string } = JSON.parse(localStorage.getItem(storageItemKey) as string)
       
+      // add item to store using dispatch and additem action
       dispatch(addItem({
         item: item.item,
         quantity: item.quantity,
@@ -28,18 +34,20 @@ function App() {
   }, [])
 
   const handleChangeItem = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // check that field is not empty
     if (e.currentTarget.value.length < 0) return
     setItem(e.target.value)
   }
 
   const handleChangeQuantity = (count: number) => {
+    // check field value type and count value
     if (!(typeof count === 'number' && count > -1)) return
     setQuantity(count)
   }
 
   const handleAddToStore = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    // check for values
+    // check for validation
     if (item.length <= 0 || quantity <= 0) return
     
     dispatch( addItem({
@@ -54,6 +62,7 @@ function App() {
   }
 
   const handleRemoveItem = (index: number) => {
+    // check for valid index number
     if (index < 0) return
     dispatch ( removeItem(index) )
   }
@@ -70,11 +79,10 @@ function App() {
         handleChangeQuantity={handleChangeQuantity} 
         handleAddToStore={handleAddToStore} />
 
-        <ItemsList 
-        handleRemoveItem={handleRemoveItem} />
+        <ItemsList handleRemoveItem={handleRemoveItem} />
       </main>
       <footer>
-        <p>Created by <a href='https://twitter.com/yourfavben' target={'_blank'}>@yourfavben 😎  </a></p>
+        <p>Crafted by <a href='https://twitter.com/yourfavben' target={'_blank'}>@yourfavben 😎  </a></p>
       </footer>
     </div>
   );
